0115805e40b1a9f625c2250b3e3468c01948c368
VPobaranchuk <vpobaranchuk@gmail.com>
2024-02-18 20:51:39 +0100

    Checkbox: in progress...

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import CheckboxDemo from '@/components/ui/checkbox/checkbox'
 
 export function App() {
   return (
-    <div>
+    <div style={{ alignItems: 'center', display: 'flex', margin: '40px' }}>
       <CheckboxDemo />
     </div>
   )
