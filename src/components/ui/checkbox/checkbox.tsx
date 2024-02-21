8d6ecf49a8ba3f5d0b7a9dcf3cd8af95f7b86780
VPobaranchuk <vpobaranchuk@gmail.com>
2024-02-21 09:59:56 +0100

    Checkbox: in progress...

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -3,12 +3,20 @@ import { CheckIcon } from '@radix-ui/react-icons'
 
 import s from './checkbox.module.scss'
 
-const CheckboxDemo = () => (
-  <Checkbox.Root className={s.CheckboxRoot}>
-    <Checkbox.Indicator className={s.CheckboxIndicator}>
-      <CheckIcon />
-    </Checkbox.Indicator>
-  </Checkbox.Root>
+type CheckboxProps = {
+  disabled?: boolean
+}
+const CheckboxDemo = ({ disabled = false }: CheckboxProps) => (
+  <div className={s.Container}>
+    <Checkbox.Root className={s.CheckboxRoot} disabled={disabled}>
+      <Checkbox.Indicator className={s.CheckboxIndicator}>
+        <CheckIcon />
+      </Checkbox.Indicator>
+    </Checkbox.Root>
+    <label className={s.Label} data-disabled={disabled}>
+      Check-box
+    </label>
+  </div>
 )
 
 export default CheckboxDemo
